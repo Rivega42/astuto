c9387d9a2bf75c89c90dea773cf95493f5f628da
Роман Гудков <39859935+Rivega42@users.noreply.github.com>
2025-03-26 16:48:19 +0300

    переводы

--- app/workflows/create_welcome_entities_workflow.rb
+++ app/workflows/create_welcome_entities_workflow.rb
@@ -5,37 +5,37 @@ class CreateWelcomeEntitiesWorkflow
 
     # Create some Boards
     feature_board = Board.create!(
-      name: 'Feature Requests',
-      description: 'This is a **board**! Go to Site settings > Boards to customise it or add more!',
+      name: 'Запрос функций',
+      description: 'Это **доска**! Перейдите в Настройки сайта > Доски, чтобы настроить ее или добавить больше!',
       order: 0
     )
     bug_board = Board.create!(
-      name: 'Bug Reports',
-      description: 'Tell us everything about problems you encountered in our services!',
+      name: 'Отчеты об ошибках',
+      description: 'Расскажите нам обо всех проблемах, с которыми вы столкнулись при использовании наших услуг!',
       order: 1
     )
 
     # Create some Post Statuses
     planned_post_status = PostStatus.create!(
-      name: 'Planned',
+      name: 'Запланировано',
       color: '#0096ff',
       order: 0,
       show_in_roadmap: true
     )
     in_progress_post_status = PostStatus.create!(
-      name: 'In Progress',
+      name: 'В работе',
       color: '#9437ff',
       order: 1,
       show_in_roadmap: true
     )
     completed_post_status = PostStatus.create!(
-      name: 'Completed',
+      name: 'Завершено',
       color: '#6ac47c',
       order: 2,
       show_in_roadmap: true
     )
     rejected_post_status = PostStatus.create!(
-      name: 'Rejected',
+      name: 'Отклонено',
       color: '#ff2600',
       order: 3,
       show_in_roadmap: false
@@ -43,8 +43,8 @@ class CreateWelcomeEntitiesWorkflow
 
     # Create some Posts
     post1 = Post.create!(
-      title: 'This is an example feedback post, click to learn more!',
-      description: 'Users can submit feedback by publishing posts like this. You can assign a **status** to each post: this one, for example, is marked as "Planned". Remember that you can customise post statuses from Site settings > Statuses',
+      title: 'Это пример поста с отзывом, нажмите, чтобы узнать больше!',
+      description: 'Пользователи могут отправлять отзывы, публикуя посты, подобные этому. Вы можете назначить **статус** каждому посту: этот, например, отмечен как «Запланировано». Помните, что вы можете настраивать статусы постов в разделе Настройки сайта > Статусы',
       board_id: feature_board.id,
       user_id: owner.id,
       post_status_id: planned_post_status.id
@@ -56,15 +56,15 @@ class CreateWelcomeEntitiesWorkflow
     )
 
     post2 = Post.create!(
-      title: 'There are multiple boards',
-      description: 'We created two boards for you, "Feature Requests" and "Bug Reports", but you can add or remove as many as you want! Just head to Site settings > Boards!',
+      title: 'Есть несколько досок',
+      description: 'Мы создали для вас две доски: «Запросы функций» и «Отчеты об ошибках», но вы можете добавлять или удалять столько, сколько захотите! Просто перейдите в Настройки сайта > Доски!',
       board_id: bug_board.id,
       user_id: owner.id
     )
 
     # Create some comments
     post1.comments.create!(
-      body: 'Users can comment to express their opinions! As with posts and board descriptions, comments can be *Markdown* **formatted**',
+      body: 'Пользователи могут комментировать, чтобы выразить свое мнение! Как и в случае с сообщениями и описаниями досок, комментарии могут быть *Markdown* **отформатированы**',
       user_id: owner.id
     )
 
